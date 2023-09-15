08a8b92f9fcb8c8818b5d11dcd105aeef30ce65a
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-09-15 20:56:52 -0300

    change disabled pattern to false

--- src/app/components/general/default-button/default-button.component.ts
+++ src/app/components/general/default-button/default-button.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class DefaultButtonComponent {
 
-  @Input() disabled: boolean = true;
+  @Input() disabled: boolean = false;
   @Input() classes: string = '';
   @Output('action') action: EventEmitter<any> = new EventEmitter();
 
