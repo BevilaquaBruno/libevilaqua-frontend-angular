8f397fc96fe6e5cb9021eac7a0f2e7d8efa66202
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2023-12-31 12:55:31 -0300

    create type crud

---
+++ src/app/components/tags/list-tags/list-tags.component.ts
@@ -0,0 +1,102 @@
+import { Component, ViewChild } from '@angular/core';
+import { TagInterface } from '../tag.interface';
+import { ConfirmDialogComponent } from '../../general/confirm-dialog/confirm-dialog.component';
+import { TagsService } from '../tags.service';
+import { AppService } from 'src/app/app.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-tags',
+  templateUrl: './list-tags.component.html'
+})
+export class ListTagsComponent {
+  listTags: TagInterface[] = [];
+  currentPage: number = 1;
+  itensPerPage: number = 10;
+  maxPages: number = 0;
+  maxRegisters: number = 0;
+  deleteData = { id: 0 };
+
+  @ViewChild(ConfirmDialogComponent) confirmationDialog!: ConfirmDialogComponent;
+
+  constructor(
+    private service: TagsService,
+    private appService: AppService,
+    private router: Router
+  ) {
+    this.appService.isValid().subscribe(
+      () => { },
+      (error) => {
+        if (error.status === 401) this.router.navigate(['/login']);
+      }
+    );
+    const storageItensPerPage = localStorage.getItem('itensPerPage');
+    if (storageItensPerPage != null) {
+      this.itensPerPage = parseInt(storageItensPerPage);
+    }
+  }
+
+  ngOnInit(): void {
+    this.updateTagList();
+  }
+
+  openNewTag() {
+    this.router.navigate(['/tags/cadastrar']);
+  }
+
+  openConfirmDeleteTag(id: number) {
+    this.deleteData.id = id;
+    this.confirmationDialog.openModal();
+  }
+
+  finishDeleteTag() {
+    this.deleteData.id = 0;
+    this.confirmationDialog.closeModal();
+    this.updateTagList();
+  }
+
+  confirmDeleteTag() {
+    if (this.deleteData.id != 0) {
+      this.service.delete(this.deleteData.id).subscribe(
+        (success) => {
+          if (success.affected != 0) {
+            this.finishDeleteTag();
+          }
+        }, (error) => {
+          console.log('Erro ao deletar tag');
+        });
+    }
+  }
+
+  updateTagList() {
+    this.service
+      .list(this.currentPage, this.itensPerPage)
+      .subscribe((types) => {
+        this.maxRegisters = types.count;
+        this.listTags = types.data;
+        let calcMaxPage = Math.ceil(this.maxRegisters / this.itensPerPage);
+        this.maxPages = calcMaxPage;
+      });
+  }
+
+  nextPage() {
+    if (this.maxPages > this.currentPage) {
+      this.currentPage++;
+      this.updateTagList();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage !== 1) {
+      this.currentPage--;
+      this.updateTagList();
+    }
+  }
+
+  itensPerPageChanged() {
+    localStorage.setItem('itensPerPage', this.itensPerPage.toString());
+    this.currentPage = 1;
+    this.updateTagList();
+  }
+
+}
--- src/app/components/tags/tags.service.ts
+++ src/app/components/tags/tags.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { constants } from 'src/environments/environments';
-import { TagListInterface } from './tag.interface';
+import { TagCreateInterface, TagDeleteInterface, TagInterface, TagListInterface, TagUpdateInterface, TagUpdateResponseInterface } from './tag.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +23,21 @@ export class TagsService {
       params,
       headers: this.headers,
     });
+  }
+
+  delete(id: number): Observable<TagDeleteInterface> {
+    return this.http.delete<TagDeleteInterface>(`${this.API}/${id}`, { headers: this.headers });
+  }
+
+  create(type: TagCreateInterface): Observable<TagInterface> {
+    return this.http.post<TagInterface>(this.API, type, { headers: this.headers });
+  }
+
+  get(id: number): Observable<TagInterface> {
+    return this.http.get<TagInterface>(`${this.API}/${id}`, { headers: this.headers });
+  }
+
+  update(id:number, type: TagUpdateInterface): Observable<TagUpdateResponseInterface> {
+    return this.http.patch<TagUpdateResponseInterface>(`${this.API}/${id}`,type, { headers: this.headers });
   }
 }
