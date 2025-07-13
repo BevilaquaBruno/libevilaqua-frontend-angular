66dab26f2f26237212a80554548c6bd4f074b3cd
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-07-12 21:03:18 -0300

    add environment.ts

---
+++ src/environments/environments.ts
@@ -0,0 +1,38 @@
+export const environment = {
+  production: false,
+  title: 'Libevilaqua'
+};
+
+export const constants = {
+  APP_URL: "https://api.libevilaqua.bevilaqua.dev.br",
+};
+
+export enum StateList {
+  AC = 'AC',
+  AL = 'AL',
+  AP = 'AP',
+  AM = 'AM',
+  BA = 'BA',
+  CE = 'CE',
+  DF = 'DF',
+  ES = 'ES',
+  GO = 'GO',
+  MA = 'MA',
+  MT = 'MT',
+  MS = 'MS',
+  MG = 'MG',
+  PA = 'PA',
+  PB = 'PB',
+  PR = 'PR',
+  PE = 'PE',
+  PI = 'PI',
+  RJ = 'RJ',
+  RN = 'RN',
+  RS = 'RS',
+  RO = 'RO',
+  RR = 'RR',
+  SC = 'SC',
+  SP = 'SP',
+  SE = 'SE',
+  TO = 'TO',
+}
